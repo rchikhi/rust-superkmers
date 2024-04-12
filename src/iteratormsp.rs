cb4d1bdb1f61730fb3278fe207ee7aeab4ca4252
Rayan <rayan.chikhi@pasteur.fr>
2024-04-12 11:43:55 +0900

    added l=10

--- src/iteratormsp.rs
+++ src/iteratormsp.rs
@@ -1,54 +1,58 @@
-use crate::Superkmer; 
+use crate::Superkmer;
 use debruijn::dna_string::DnaString;
-use debruijn::kmer::{Kmer8, Kmer12};
+use debruijn::kmer::{Kmer8, Kmer10, Kmer12};
 use debruijn::Kmer;
-use debruijn::msp::{Scanner};
+use debruijn::msp::Scanner;
 
 pub struct SuperkmersIterator<'a> {
     iter: Box<dyn Iterator<Item = Superkmer> + 'a>,
 }
 
-
-
  /* wrapper around rust-debruijn msp funtions
   */
 impl<'a> SuperkmersIterator<'a> {
     pub fn new(dnastring: &'a DnaString, k: usize, l: usize) -> Self {
-            let superkmer_iter : Box<dyn Iterator<Item = Superkmer>>;
-            if l == 8 {
+        let superkmer_iter: Box<dyn Iterator<Item = Superkmer>> = match l {
+            8 => {
                 let score8 = |p: &Kmer8| p.to_u64() as usize;
                 let scanner8 = Scanner::new(dnastring, score8, k);
-				superkmer_iter = Box::new(scanner8.scan().into_iter().map(|msp| Superkmer {
-					start: msp.start as usize,
-					size: msp.len as u8,
-					mpos: (msp.minimizer_pos - msp.start) as u8,
-					rc: false,
-					mseq: msp.minimizer.to_string(),
-				}));
+                Box::new(scanner8.scan().into_iter().map(|msp| Superkmer {
+                    start: msp.start as usize,
+                    size: msp.len as u8,
+                    mpos: (msp.minimizer_pos - msp.start) as u8,
+                    rc: false,
+                    mseq: msp.minimizer.to_string(),
+                }))
             }
-            else {  if l == 12 {
+            10 => {
+                let score10 = |p: &Kmer10| p.to_u64() as usize;
+                let scanner10 = Scanner::new(dnastring, score10, k);
+                Box::new(scanner10.scan().into_iter().map(|msp| Superkmer {
+                    start: msp.start as usize,
+                    size: msp.len as u8,
+                    mpos: (msp.minimizer_pos - msp.start) as u8,
+                    rc: false,
+                    mseq: msp.minimizer.to_string(),
+                }))
+            }
+            12 => {
                 let score12 = |p: &Kmer12| p.to_u64() as usize;
                 let scanner12 = Scanner::new(dnastring, score12, k);
-				superkmer_iter = Box::new(scanner12.scan().into_iter().map(|msp| Superkmer {
-					start: msp.start as usize,
-					size: msp.len as u8,
-					mpos: (msp.minimizer_pos - msp.start) as u8,
-					rc: false,
-					mseq: msp.minimizer.to_string(),
-				}));
+                Box::new(scanner12.scan().into_iter().map(|msp| Superkmer {
+                    start: msp.start as usize,
+                    size: msp.len as u8,
+                    mpos: (msp.minimizer_pos - msp.start) as u8,
+                    rc: false,
+                    mseq: msp.minimizer.to_string(),
+                }))
             }
-            else {
-                panic!("unsupported l size for MSP iteration");
-            } }
+            _ => panic!("Unsupported l size for MSP iteration"),
+        };
 
-            SuperkmersIterator {
-                iter: superkmer_iter,
-            }
+        SuperkmersIterator { iter: superkmer_iter }
     }
 }
 
-
-// Implementing Iterator for SuperkmersIterator
 impl<'a> Iterator for SuperkmersIterator<'a> {
     type Item = Superkmer;
 
@@ -56,3 +60,4 @@ impl<'a> Iterator for SuperkmersIterator<'a> {
         self.iter.next()
     }
 }
+
